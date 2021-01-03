323ccfa4888ed62787af25900cc060eb84dc17ed
Tobias <masterarbeithhz@gmail.com>
2021-01-03 18:19:11 +0100

    sf

--- image_files/pipeline.php
+++ image_files/pipeline.php
@@ -10,10 +10,15 @@
 		<h2>Jenkins trigger page</h2>
 	</div>
 
-	<a href="http://35.204.41.212/job/BaseArchitecture_MultiUserLogin/buildWithParameters?token=ajsklfj65465asfd465a65sd4f6a4f654asf6a4sfd64asf6&base_groovy=base_config">BaseArchitecture_MultiUserLogin</a>
+	<form action="http://35.204.41.212/job/BaseArchitecture_MultiUserLogin/buildWithParameters?token=ajsklfj65465asfd465a65sd4f6a4f654asf6a4sfd64asf6&base_groovy=base_config" method="post" target="_blank">
+		<button type="submit">Submit</button>
+		<button type="submit" formmethod="post">Submit using POST</button>
+	</form>
+
+	<!-- <a href="http://35.204.41.212/job/BaseArchitecture_MultiUserLogin/buildWithParameters?token=ajsklfj65465asfd465a65sd4f6a4f654asf6a4sfd64asf6&base_groovy=base_config">BaseArchitecture_MultiUserLogin	</a> -->
 
 	
-
+	
 </body>
 
 </html>
