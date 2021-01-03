839e5d988c24dd4bd52e6543646498527e8cd548
Tobias <masterarbeithhz@gmail.com>
2021-01-03 16:53:35 +0100

    as

--- image_files/pipeline.php
+++ image_files/pipeline.php
@@ -13,7 +13,7 @@
 		<input type="submit">
 	</form>
 
-	<form action="http://35.204.41.212/job/BaseArchitecture_MultiUserLogin/buildWithParameters?token=ajsklfj65465asfd465a65sd4f6a4f654asf6a4sfd64asf6&base_groovy=base_config" method="get" target="_blank">
+	<form action="http://35.204.41.212/job/BaseArchitecture_MultiUserLogin/buildWithParameters?token=ajsklfj65465asfd465a65sd4f6a4f654asf6a4sfd64asf6&base_groovy=base_config" method="get" target="_blank" enctype="application/x-www-form-urlencoded">
 		<button type="submit">Submit</button>
 		<button type="submit" formmethod="post">Submit using POST</button>
 	</form>
