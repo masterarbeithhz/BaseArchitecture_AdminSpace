391d590d57c0cc9f74684bb21e9461b6fddd281a
Tobias <masterarbeithhz@gmail.com>
2021-01-03 16:23:44 +0100

    a

--- image_files/Jenkins.php
+++ image_files/Jenkins.php
@@ -9,9 +9,6 @@
 	<div class="header">
 		<h2>Jenkins trigger page</h2>
 	</div>
-
-	<input type="submit" value="Start" onclick="submit()">
-
 	<form action="http://35.204.41.212/job/BaseArchitecture_MultiUserLogin/buildWithParameters?token=ajsklfj65465asfd465a65sd4f6a4f654asf6a4sfd64asf6&base_groovy=base_config" method="POST">
 		<input type="submit">
 	</form>
