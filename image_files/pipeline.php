1761e8c6254a0b5c41cb594d96ed3824c68e10de
Tobias <masterarbeithhz@gmail.com>
2021-01-03 18:55:41 +0100

    as

--- image_files/pipeline.php
+++ image_files/pipeline.php
@@ -9,12 +9,11 @@
 	<div class="header">
 		<h2>Jenkins trigger page</h2>
 	</div>
-
-	<form action="http://35.204.41.212/job/BaseArchitecture_MultiUserLogin/buildWithParameters" method="get" target="_blank">
+	<iframe id="iframe" name="my_iframe"></iframe>
+	<form action="http://35.204.41.212/job/BaseArchitecture_MultiUserLogin/buildWithParameters" method="get" target="my_iframe">
 		<input type="hidden" name="token" value="ajsklfj65465asfd465a65sd4f6a4f654asf6a4sfd64asf6">
 		<input type="hidden" name="base_groovy" value="base_config">
-		<button type="submit">Submit</button>
-		<button type="submit" formmethod="post">Submit using POST</button>
+		<button type="submit">Submit</button>		
 	</form>
 
 	<!-- <a href="http://35.204.41.212/job/BaseArchitecture_MultiUserLogin/buildWithParameters?token=ajsklfj65465asfd465a65sd4f6a4f654asf6a4sfd64asf6&base_groovy=base_config">BaseArchitecture_MultiUserLogin	</a> -->
