8bde965ebc3e0f78f5822b12a7eddc9bd8b62486
Tobias <masterarbeithhz@gmail.com>
2021-01-03 19:55:45 +0100

    ssdfgsdf

--- image_files/pipeline.php
+++ image_files/pipeline.php
@@ -1,43 +1,46 @@
-<!DOCTYPE html>
-<html>
+<?php
+if (!isset($_POST['submit']) || !isset($_POST['pipeline'])) {
+?>
 
-<head>
-	<title>Jenkins trigger</title>
-</head>
+	<!DOCTYPE html>
+	<html>
 
-<body>
-	<div class="header">
-		<h2>Jenkins trigger page</h2>
-	</div>
+	<head>
+		<title>Jenkins trigger</title>
+	</head>
 
-	<!-- <form action="http://35.204.41.212/job/BaseArchitecture_MultiUserLogin/buildWithParameters?token=ajsklfj65465asfd465a65sd4f6a4f654asf6a4sfd64asf6&base_groovy=base_config" method="get" target="_blank" enctype="application/x-www-form-urlencoded">
-		<button type="submit">Submit</button>
-		<button type="submit" formmethod="post">Submit using POST</button>
-	</form> -->
+	<body>
+		<div class="header">
+			<h2>Jenkins trigger page</h2>
+		</div>
 
+		<form action="http://130.211.228.246/base/admin/pipeline.php" method="post" target="_blank" enctype="application/x-www-form-urlencoded">
+			<input type="hidden" name="pipeline" value="multiuserlogin">
+		    <input type="submit" name="submit" value="Submit">
+	    </form>
+	</body>
 
-
-	<?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
- // erzeuge einen neuen cURL-Handle
- $ch = curl_init();
-
- // setze die URL und andere Optionen
- curl_setopt($ch, CURLOPT_URL, "http://user:cDAbHn8YJ8xN@35.204.41.212/job/BaseArchitecture_MultiUserLogin/buildWithParameters?token=ajsklfj65465asfd465a65sd4f6a4f654asf6a4sfd64asf6&base_groovy=base_config");
- curl_setopt($ch, CURLOPT_HEADER, 0);
-
- // führe die Aktion aus und gib die Daten an den Browser weiter
- curl_exec($ch);
-
- // schließe den cURL-Handle und gib die Systemresourcen frei
- curl_close($ch);
- 
+	</html>
+<?php
+} else {
 
 
+	ini_set('display_errors', '1');
+	ini_set('display_startup_errors', '1');
+	error_reporting(E_ALL);
+	// erzeuge einen neuen cURL-Handle
+	$ch = curl_init();
+
+	// setze die URL und andere Optionen
+	curl_setopt($ch, CURLOPT_URL, "http://user:cDAbHn8YJ8xN@35.204.41.212/job/BaseArchitecture_MultiUserLogin/buildWithParameters?token=ajsklfj65465asfd465a65sd4f6a4f654asf6a4sfd64asf6&base_groovy=base_config");
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+
+	// führe die Aktion aus und gib die Daten an den Browser weiter
+	curl_exec($ch);
+
+	// schließe den cURL-Handle und gib die Systemresourcen frei
+	curl_close($ch);
+	echo "<h1>Erzeugt</h1>";
+}
 
 ?>
-</body>
-
-</html>
